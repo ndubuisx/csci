3aeed602c2ea20b024576e5d5f5c2298789ed441
Ndubuisi <mcndubuisi@icloud.com>
2018-09-27 21:46:32 -0400

    Add lab and note files

---
+++ lab/HelloWorld.java
@@ -0,0 +1,22 @@
+/******************************************************************************
+ *
+ *  Emmanuel Ndubuisi
+ *  CSCI 206 - 1L
+ *  Lab 1 - HelloWorld.java
+ *  August 31, 2018
+ 
+ 
+ *  Compilation:  javac HelloWorld.java
+ *  Execution:    java HelloWorld
+ *  Prints "Hello, World" to the terminal
+ *
+ ******************************************************************************/
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
---
+++ lab/Shapes.java
@@ -0,0 +1,28 @@
+/******************************************************************************
+ *
+ *  Emmanuel Ndubuisi
+ *  CSCI 206 - 1L
+ *  Lab 2 - Shapes.java
+ *  September 11, 2018
+ 
+ 
+ *  Compilation:  javac Shapes.java
+ *  Execution:    java Shapes
+ *  Prints Shapes with Asterisks to the terminal
+ *
+ ******************************************************************************/
+ 
+public class Shapes {
+    public static void main (String [] args) {
+        // Print a box, an oval, an arrow and a diamond using asterisks (*) 
+        System.out.print ("*********      ***        *          * \n");
+        System.out.print ("*       *    *     *     ***        * * \n");
+        System.out.print ("*       *   *       *   *****      *   * \n");
+        System.out.print ("*       *   *       *     *       *     * \n");
+        System.out.print ("*       *   *       *     *      *       * \n");
+        System.out.print ("*       *   *       *     *       *     * \n");
+        System.out.print ("*       *   *       *     *        *   * \n");
+        System.out.print ("*       *    *     *      *         * * \n");
+        System.out.print ("*********      ***        *          * \n");
+    }
+}
